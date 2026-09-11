3797ce844cc320fa89cc74d40fd47692f35c8fbb
Ana V <anafernandagv27@gmail.com>
2026-09-10 20:37:42 -0600

    App actualizado para mostrar botones de 2 vistas

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
 import { RegisterForm} from './components/auth/RegisterForm';
+import { useState } from 'react'
+import { ProductList } from './components/products/ProductList'
 
 function App(){
-  return <RegisterForm/>;
-}
+  // Cambiar vistas entre registro y catologo 
+  const [vistaActual, setVistaActual] = useState('registro');
 
+  return(
+    <div>
+      <nav>
+        <button onClick={() => setVistaActual('registro')}>Crear Cuenta</button>
+        <button onClick={() => setVistaActual('catalogo')}>Ver Catalogo</button>
+      </nav>
+
+      {/*Mostrar el componente segun la vista */}
+      {vistaActual === 'registro' && <RegisterForm/>}
+      {vistaActual === 'catalogo' && <ProductList/>}
+    </div>
+  );
+}
 export default App;
